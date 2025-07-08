cf2a5c6d22a6bc81d919a2b245f1ff1c1c24e4cc
RiaVenice <riatcruz.v1@gmail.com>
2025-07-08 23:01:30 +0800

    refractor: change path name dbpostgre

--- utils/dbResetPostgresql.util.php
+++ utils/dbResetPostgresql.util.php
@@ -9,7 +9,7 @@ require 'vendor/autoload.php';
 require 'bootstrap.php';
 
 // 3) envSetter
-require_once UTILS_PATH . '/envSetter.util.php';
+require_once __DIR__ . '/envSetter.util.php';
 
 // ——— Connect to PostgreSQL ———
 $dsn = "pgsql:host={$pgConfig['host']};port={$pgConfig['port']};dbname={$pgConfig['db']}";
