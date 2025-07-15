5eaee3d6bb4068a47f4d67a9d2bb4d69d911ce8a
RiaVenice <riatcruz.v1@gmail.com>
2025-07-15 18:29:33 +0800

    feat(users.staticData.php): added dummy data for users

---
+++ staticDatas/dummies/users.staticData.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    ['username' => 'airven', 'first_name' => 'Venice', 'middle_name' => null, 'last_name' => 'Cruz', 'password' => 'p@ssW0rd5678', 'role' => 'team lead'],
+    ['username' => '1ensh1', 'first_name' => 'Roma', 'middle_name' => null, 'last_name' => 'Papa', 'password' => 'p@ssW0rd1234', 'role' => 'designer'],
+    ['username' => 'skullpanda', 'first_name' => 'Hirono', 'middle_name' => null, 'last_name' => 'Summerbreak', 'password' => 'p@ssW0rd9876', 'role' => 'back end'],
+];
