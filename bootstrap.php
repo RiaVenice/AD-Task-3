5e394e4155b0710fa905d9e8e7e64c95ec382181
RiaVenice <riatcruz.v1@gmail.com>
2025-07-15 18:21:00 +0800

    feat(bootstrap.php): added more paths

--- bootstrap.php
+++ bootstrap.php
@@ -1,9 +1,18 @@
 <?php
-define('BASE_PATH', realpath(__DIR__));
+if (!defined('BASE_PATH')) {
+    define('BASE_PATH', realpath(__DIR__));
+}
 define('HANDLERS_PATH', realpath(BASE_PATH . "/handlers"));
 define('UTILS_PATH', realpath(BASE_PATH . "/utils"));
-define('TEMPLATES_PATH', realpath(BASE_PATH . "/components/templates"));
-define('LAYOUTS_PATH', realpath(BASE_PATH . "/layouts"));
 define('DUMMIES_PATH', realpath(BASE_PATH . "/staticDatas/dummies"));
+define('COMPONENTS_PATH', realpath(BASE_PATH . "/components"));
+define('LAYOUTS_PATH', realpath(BASE_PATH . "/layouts"));
+define('PAGES_PATH', realpath(BASE_PATH . "/pages"));
+define('STATICDATAS_PATH', realpath(BASE_PATH . "/staticDatas"));
+define('ERRORS_PATH', realpath(BASE_PATH . "/errors"));
+define('SQL_PATH', realpath(BASE_PATH . "/sql"));
+define('TEMPLATES_PATH', realpath(BASE_PATH . "/components/templates"));
+define('COMPONENTS_GROUP_PATH', realpath(BASE_PATH . "/components/componentGroup"));
+define('DATABASE_PATH', realpath(BASE_PATH . "/database"));
 
 chdir(BASE_PATH);
