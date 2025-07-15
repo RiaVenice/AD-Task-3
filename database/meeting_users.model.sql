57feee7be25f70a6b378e229765fb9ff74f0af47
RiaVenice <riatcruz.v1@gmail.com>
2025-07-15 18:05:41 +0800

    refractor(meeting_users.model.sql): changed schema

--- database/meeting_users.model.sql
+++ database/meeting_users.model.sql
@@ -1,6 +1,6 @@
-CREATE TABLE IF NOT EXISTS public."meeting_users" (
-    id uuid NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
-    role varchar(50) DEFAULT 'attendee',
-    status varchar(50) DEFAULT 'invited',
-    invited_at timestamp DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE IF NOT EXISTS meeting_users (
+    meeting_id uuid NOT NULL REFERENCES meetings (id),
+    user_id uuid NOT NULL REFERENCES users (id),
+    role VARCHAR(50) NOT NULL,
+    PRIMARY KEY (meeting_id, user_id)
 );
