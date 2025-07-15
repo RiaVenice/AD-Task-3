96e3b5bc834422a0434c26b6bff942e29380908e
RiaVenice <riatcruz.v1@gmail.com>
2025-07-15 18:53:43 +0800

    feat(users.staticData.php): added IDs

--- staticDatas/dummies/users.staticData.php
+++ staticDatas/dummies/users.staticData.php
@@ -1,6 +1,31 @@
 <?php
+// users.staticData.php
 return [
-    ['username' => 'airven', 'first_name' => 'Venice', 'middle_name' => null, 'last_name' => 'Cruz', 'password' => 'p@ssW0rd5678', 'role' => 'team lead'],
-    ['username' => '1ensh1', 'first_name' => 'Roma', 'middle_name' => null, 'last_name' => 'Papa', 'password' => 'p@ssW0rd1234', 'role' => 'designer'],
-    ['username' => 'skullpanda', 'first_name' => 'Hirono', 'middle_name' => null, 'last_name' => 'Summerbreak', 'password' => 'p@ssW0rd9876', 'role' => 'back end'],
+    [
+        'id' => '2fb5d1de-425c-4ee3-a8ec-84c44b650796',
+        'username' => 'airven', 
+        'first_name' => 'Venice', 
+        'middle_name' => null, 
+        'last_name' => 'Cruz', 
+        'password' => 'p@ssW0rd5678', 
+        'role' => 'team lead'
+    ],
+    [
+        'id' => '66892bd6-2c31-4497-a382-3a76784f42b0',
+        'username' => '1ensh1', 
+        'first_name' => 'Roma', 
+        'middle_name' => null, 
+        'last_name' => 'Papa', 
+        'password' => 'p@ssW0rd1234', 
+        'role' => 'designer'
+    ],
+    [
+        'id' => 'c3f8d2a1-9e4b-4c6d-8f3a-1b5e7c9d2a4f',
+        'username' => 'skullpanda', 
+        'first_name' => 'Hirono', 
+        'middle_name' => null, 
+        'last_name' => 'Summerbreak', 
+        'password' => 'p@ssW0rd9876', 
+        'role' => 'back end'
+    ],
 ];
