044419ac50243ad87d70ea5d7a6f77d656105101
RiaVenice <riatcruz.v1@gmail.com>
2025-07-15 19:10:55 +0800

    refractor(bootstrap.php): removes not needed paths

--- bootstrap.php
+++ bootstrap.php
@@ -4,15 +4,8 @@ if (!defined('BASE_PATH')) {
 }
 define('HANDLERS_PATH', realpath(BASE_PATH . "/handlers"));
 define('UTILS_PATH', realpath(BASE_PATH . "/utils"));
-define('DUMMIES_PATH', realpath(BASE_PATH . "/staticDatas/dummies"));
-define('COMPONENTS_PATH', realpath(BASE_PATH . "/components"));
-define('LAYOUTS_PATH', realpath(BASE_PATH . "/layouts"));
-define('PAGES_PATH', realpath(BASE_PATH . "/pages"));
-define('STATICDATAS_PATH', realpath(BASE_PATH . "/staticDatas"));
-define('ERRORS_PATH', realpath(BASE_PATH . "/errors"));
-define('SQL_PATH', realpath(BASE_PATH . "/sql"));
 define('TEMPLATES_PATH', realpath(BASE_PATH . "/components/templates"));
-define('COMPONENTS_GROUP_PATH', realpath(BASE_PATH . "/components/componentGroup"));
-define('DATABASE_PATH', realpath(BASE_PATH . "/database"));
+define('LAYOUTS_PATH', realpath(BASE_PATH . "/layouts"));
+define('DUMMIES_PATH', realpath(BASE_PATH . "/staticDatas/dummies"));
 
 chdir(BASE_PATH);
