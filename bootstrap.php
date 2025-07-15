0c91915a796b8ea2042a567990a5a28af8ba7fc9
RiaVenice <riatcruz.v1@gmail.com>
2025-07-15 18:11:32 +0800

    feat(bootstrap.php): added more paths

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,9 @@
 <?php
 define('BASE_PATH', realpath(__DIR__));
-
 define('HANDLERS_PATH', realpath(BASE_PATH . "/handlers"));
+define('UTILS_PATH', realpath(BASE_PATH . "/utils"));
+define('TEMPLATES_PATH', realpath(BASE_PATH . "/components/templates"));
+define('LAYOUTS_PATH', realpath(BASE_PATH . "/layouts"));
+define('DUMMIES_PATH', realpath(BASE_PATH . "/staticDatas/dummies"));
 
 chdir(BASE_PATH);
